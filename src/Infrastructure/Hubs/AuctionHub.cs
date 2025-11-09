a46db6b7a8e03c903ebccb0994f50d61dc094e24
Eliton Peroni <eliton_peroni@hotmail.com>
2025-11-09 12:40:16 -0300

    adjustments hub

--- src/Infrastructure/Hubs/AuctionHub.cs
+++ src/Infrastructure/Hubs/AuctionHub.cs
@@ -11,25 +11,9 @@ using Microsoft.Extensions.Logging;
 
 namespace Infrastructure.Hubs;
 
-
-// Assuma que você tem um serviço para interagir com o banco de dados/lógica de leilão
-// Exemplo: ILeilaoService
 //[Authorize]
-public class AuctionHub : Hub
+public class AuctionHub(ICommandHandler<CreateAuctionBidCommand, Guid> handler) : Hub
 {
-    private readonly ICommandHandler<CreateAuctionBidCommand, Guid> _handler;
-
-    // O Handler é injetado, pois ele está na Camada de Aplicação
-    public AuctionHub(ICommandHandler<CreateAuctionBidCommand, Guid> handler)
-    {
-        _handler = handler;
-    }
-
-    /// <summary>
-    /// Método chamado quando o cliente Next.js se conecta.
-    /// Ele deve chamar Clients.Group(productGroupName).SendAsync.
-    /// </summary>
-    /// <param name="groupName">O ID do produto (ex: "product_1") que o cliente está visualizando.</param>
     public async Task JoinAuctionGroup(string groupName)
     {
         // 1. Adiciona a conexão atual (o cliente) ao grupo específico do produto.
@@ -39,16 +23,7 @@ public class AuctionHub : Hub
         // Opcional: Enviar uma mensagem de boas-vindas ou o estado atual do leilão
         await Clients.Caller.SendAsync("ReceiveMessage", "AuctionHub", $"Você entrou no leilão: {groupName}");
     }
-
-
-    /// <summary>
-    /// Recebe o novo lance do cliente Next.js (via invoke).
-    /// Assinatura do cliente Next.js: connection.invoke("SendBid", groupName, user, bidAmount.toString())
-    /// </summary>
-    /// <param name="auctionId">O grupo/ID do produto (ex: "product_1").</param>
-    /// <param name="user">O ID ou nome do usuário que fez o lance.</param>
-    /// <param name="bidValueString">O valor do lance como string.</param>
-    public async Task SendBid(Guid auctionId, string bidValueString)
+    public async Task SendBid(string groupName, string bidValueString)
     {
         //var userId = Guid.Parse(Context?.User?.FindFirst(ClaimTypes.NameIdentifier)?.Value ?? Guid.Empty.ToString());
         string userName = Context?.User?.Identity?.Name ?? "Licitante Desconhecido";
@@ -64,44 +39,28 @@ public class AuctionHub : Hub
             await Clients.Caller.SendAsync("BidError", "Valor do lance inválido.");
             return;
         }
-
-        // --- 1. Lógica de Processamento do Lance (Simulação) ---
-        // Na vida real, você chamaria _leilaoService.ProcessBid(productGroupName, user, bidAmount);
-        
-
-        await _handler.Handle(new CreateAuctionBidCommand
+        await handler.Handle(new CreateAuctionBidCommand
         {
-            AuctionId = Guid.Parse("863ca937-a969-4938-af92-11dd82303420"), //auctionId,
-            UserId = Guid.Parse("2c6be064-1db6-408a-a1ed-1583bf5dcac4"), //userId,
+            AuctionId = Guid.Parse(groupName), //auctionId,
+            UserId = Guid.Parse("52cdb602-8bf5-4f9f-8c93-c2b19a99a152"), //userId,
             BidPrice = bidAmount,
         }, CancellationToken.None);
 
-
-        // Simulação de Obtenção de Dados Pós-Lance:
-        // Na realidade, esses valores viriam do seu serviço/banco de dados após um lance bem-sucedido.
         decimal newCurrentBid = bidAmount;
         int newTotalBids = 20 + 1;
         //string newBidderName = userName;
         string newBidTime = DateTime.Now.ToString("HH:mm:ss", System.Globalization.CultureInfo.InvariantCulture);
-        // --- 2. Broadcast para os Clientes Next.js ---
 
-        // CONTRATO: SendAsync("SendBid", receivedProductId, newBidAmount, newTotalBids, newBidderName, newBidTime)
-
-        await Clients.Group(auctionId.ToString()).SendAsync(
-            "ReceiveNewBid",                 // Nome do método no cliente (newConnection.on("SendBid", ...))
-            auctionId,                 // 1. ID do Produto (number)
-            newCurrentBid,             // 2. Novo Valor do Lance (number)
-            newTotalBids,              // 3. Novo Total de Lances (number)
-            userName,             // 4. Nome do Licitante (string)
-            newBidTime                 // 5. Tempo do Lance (string)
-        );
-
-        //_logger.LogInformation($"Lance de {newCurrentBid} enviado para o grupo {productGroupName} por {user}.");
-    
-    
+        await Clients.Group(groupName.ToString()).SendAsync(
+            ChannelNames.ReceiveNewBid,
+            groupName,                
+            newCurrentBid,            
+            newTotalBids,              
+            userName,             
+            newBidTime                 
+        );    
     }
 
-    // Opcional: Remover do grupo quando o cliente desconectar (ou fechar a aba)
     public override async Task OnDisconnectedAsync(Exception? exception)
     {
         // A lógica de remoção do grupo pode ser mais complexa se houver muitos grupos
