b95868306cdbf662871c3f87f45a9e2cc15be414
Eliton Peroni <eliton.peroni@kognit.com>
2026-02-14 17:55:46 -0300

    adjustments notification

--- src/Application/DTOs/NotificationItem.cs
+++ src/Application/DTOs/NotificationItem.cs
@@ -10,6 +10,7 @@ public class NotificationItem
 {
     public Guid Id { get; set; } = Guid.NewGuid();
     public string Message { get; set; } = string.Empty;
+    public Guid? AuctionId { get; set; } 
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     public bool IsRead { get; set; } 
 }
